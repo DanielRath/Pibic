758877a3f10d48dafe7e505d52dbf2c76ac410f1
DanielRath <danielr4th@gmail.com>
2019-05-06 23:17:58 -0300

    msg

--- testemp3.c
+++ testemp3.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+int bpm=0;
 
 int main ()
 {
+	printf("Digite o valor de seu batimento cardíaco\n");
+	scanf("%d", &bpm);
+	if (bpm > 70)
         system("mpg123 Canon.mp3");
         return 0;
 }
